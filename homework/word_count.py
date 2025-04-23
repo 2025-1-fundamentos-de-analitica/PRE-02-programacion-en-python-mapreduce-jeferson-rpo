d101a13cb7e5e68a24032ed2c4b239b13ad219aa
jeferson-rpo <jorestrepob@unal.edu.co>
2025-04-23 11:15:59 -0500

    intento 1

--- homework/word_count.py
+++ homework/word_count.py
@@ -2,6 +2,7 @@
 
 # pylint: disable=broad-exception-raised
 
+import string
 import fileinput
 import glob
 import os.path
@@ -17,8 +18,23 @@ from itertools import groupby
 # original se llama text0.txt, el archivo generado se llamará text0_1.txt,
 # text0_2.txt, etc.
 #
+
+
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
+
+    if not os.path.exists("files/input"):
+        os.makedirs("files/input")
+    for file in glob.glob("files/raw/*"):
+        for i in range(1, n + 1):
+            with open(file, "r", encoding="utf-8") as f:
+                with open(
+                    f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f2:
+                    f2.write(f.read())
+                    
 
 
 #
@@ -39,6 +55,13 @@ def copy_raw_files_to_input_folder(n):
 def load_input(input_directory):
     """Funcion load_input"""
 
+    sequence = []
+    files = glob.glob(f"{input_directory}/*")
+    with fileinput.input(files=files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
+
 
 #
 # Escriba la función line_preprocessing que recibe una lista de tuplas de la
@@ -47,7 +70,11 @@ def load_input(input_directory):
 #
 def line_preprocessing(sequence):
     """Line Preprocessing"""
-
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation)).lower())
+        for key, value in sequence
+    ]
+    return sequence
 
 #
 # Escriba una función llamada maper que recibe una lista de tuplas de la
@@ -63,6 +90,7 @@ def line_preprocessing(sequence):
 #
 def mapper(sequence):
     """Mapper"""
+    return [(word, 1) for _, value in sequence for word in value.split()]
 
 
 #
@@ -78,6 +106,9 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
+
+
 
 
 #
@@ -88,7 +119,10 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
-
+    result = []
+    for key, group in groupby(sequence, lambda x: x[0]):
+        result.append((key, sum(value for _, value in group)))
+    return result
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de
@@ -97,6 +131,11 @@ def reducer(sequence):
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
 
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 #
 # Escriba la función save_output, la cual almacena en un archivo de texto
@@ -108,6 +147,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -116,15 +158,29 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
-
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
+    
+    
 def run_job(input_directory, output_directory):
     """Job"""
 
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
 
+
+
+    
 if __name__ == "__main__":
 
     copy_raw_files_to_input_folder(n=1000)
@@ -137,4 +193,7 @@ if __name__ == "__main__":
     )
 
     end_time = time.time()
+    
+    
+    
     print(f"Tiempo de ejecución: {end_time - start_time:.2f} segundos")
